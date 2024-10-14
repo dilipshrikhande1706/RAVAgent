99902735d7eb8720f9751b073372230aefebe850
Prasad Duddi <prasad.duddi@madetech.com>
2024-10-14 16:38:03 +0100

    Tested

--- clear.sh
+++ clear.sh
@@ -1,3 +1,6 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
+kill -9 $(lsof -t -i :7860)
+kill -9 $(lsof -t -i :11343)
+
---
+++ runintel.sh
@@ -0,0 +1,3 @@
+docker pull python:3.10-slim
+docker compose up --build
+sudo docker compose up --build
